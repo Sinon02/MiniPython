20c45bbc4d76eb910c0243d53179092858118724
wmj0607 <wangmingjian@mail.ustc.edu.cn>
2019-10-25 18:15:23 +0800

    add print() for data(in list.cpp)

--- 2019-compiler-lab/list.cpp
+++ 2019-compiler-lab/list.cpp
@@ -11,10 +11,39 @@ struct symbol{
 struct data{
 	int flag;
 	union d{
-		int i;
-		float f;
-		string s;
-		struct list *l;
+		int i;//flag=0
+		float f;//flag=1
+		string s;//flag=2
+		struct list *l;//flag=3
 	};
 };
 
+void print(struct data d)
+{
+	struct data *p;
+	switch(d.flag)
+	{
+		case 0:
+			cout<<d.d.i;
+			break;
+		case 1:
+			cout<<d.d.f;
+			break;
+		case 2:
+			cout<<d.d.s;
+			break;
+		case 3:
+			cout<<'[';
+			p=d.d.l;
+			if(p!=0)
+			{
+				print(p->d);
+				for(p=p->next;p!=0;p=p->next)
+				{
+					cout<<',';
+					print(p->d);
+				}
+			}
+			cout<<']';
+	}
+}
