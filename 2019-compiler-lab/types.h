f1d95e5049a3edf3e8aba773a911093c096521fc
wmj0607 <wangmingjian@mail.ustc.edu.cn>
2019-12-09 23:04:02 +0800

    list types

--- 2019-compiler-lab/types.h
+++ 2019-compiler-lab/types.h
@@ -10,7 +10,7 @@ union vald{
 	int i;//flag=0
 	float f;//flag=1
 	char * s;//flag=2
-	struct list l;//flag=3
+	struct list *l;//flag=3
 	};
 struct VAL{
     	int flag;
@@ -32,7 +32,7 @@ union yyd{
 	int i;//0
 	float f;//1
 	char *s;//2
-	struct list l;//3
+	struct list *l;//3
 	VAL *v;//4
 	struct slice slice;//5
 	};
@@ -49,16 +49,16 @@ int FIND(char * name);
 void yyerror(const char *s);
 void print(VAL val);
 void print(YYSTYPE val);
-struct list newlist();
-void exlist(struct list &l); //extend
-void append(struct list &l,VAL _val);
-void add(struct list &l,struct list o);
+struct list* newlist();
+void exlist(struct list *l); //extend
+void append(struct list *l,VAL _val);
+void add(struct list *l,struct list *o);
 void insert(struct list &l,int index,VAL _val);
 VAL pop(struct list &l,int index);
-struct list slice(struct slice s);
-struct list slice(struct list l,int begin,int end,int step);
-inline void setslice(struct slice s,struct list o);
-void setslice(struct list &l,int begin,int end,int step,struct list o);
+struct list* slice(struct slice s);
+struct list* slice(struct list *l,int begin,int end,int step);
+inline void setslice(struct slice s,struct list *o);
+void setslice(struct list *l,int begin,int end,int step,struct list *o);
 int MOD(int n,int M);
 float FMOD(float n,float M);
 void PrintFloat(float a);
